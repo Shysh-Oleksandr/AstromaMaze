e059cb2f19b387556fb569f257ccda8a56281092
Shysh-Oleksandr <sasha.shysh23@gmail.com>
2021-06-11 21:21:39 +0300

    Added player movement.

--- Amazing Maze 3D/Assets/PlayerController.cs
+++ Amazing Maze 3D/Assets/PlayerController.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float speed = 12f;
+    private float gravity = -9.81f;
+    private CharacterController controller;
+
+    public Text winnerText;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundLayer;
+    bool isGrounded;
+
+    Vector3 velocity;
+
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundLayer);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Door")
+        {
+            Debug.Log("Win!");
+            winnerText.gameObject.SetActive(true);
+
+            Time.timeScale = 0f;
+        }
     }
 }
