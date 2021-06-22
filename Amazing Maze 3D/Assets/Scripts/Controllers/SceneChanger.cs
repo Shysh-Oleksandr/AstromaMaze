a1354b5501bb2e73bae5a6ff659385b70c066755
Shysh-Oleksandr <sasha.shysh23@gmail.com>
2021-06-22 15:53:11 +0300

    Made loading bar.

--- Amazing Maze 3D/Assets/Scripts/Controllers/SceneChanger.cs
+++ Amazing Maze 3D/Assets/Scripts/Controllers/SceneChanger.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections;
 
 public class SceneChanger : GenericSingletonClass<SceneChanger>
 {
     public Animator animator;
+    public GameObject loadingScreen;
+    public Slider slider;
+    public TextMeshProUGUI progressText;
 
     private int levelToLoad;
 
@@ -16,10 +22,38 @@ public class SceneChanger : GenericSingletonClass<SceneChanger>
     {
         levelToLoad = levelIndex;
         animator.SetTrigger("FadeOut");
+
+        switch (levelIndex)
+        {
+            case 0:
+                GameManager.Instance.UpdateGameState(GameState.MainMenu);
+                break;
+            
+        }
     }
 
     public void OnFadeComplete()
     {
-        SceneManager.LoadScene(levelToLoad);
+        StartCoroutine(LoadAsynchronously());
+    }
+
+    IEnumerator LoadAsynchronously()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(levelToLoad);
+
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+
+            slider.value = progress;
+            progressText.text = progress.ToString("0.0%");
+
+            yield return null;
+        }
+
+        loadingScreen.SetActive(false);
+
     }
 }
