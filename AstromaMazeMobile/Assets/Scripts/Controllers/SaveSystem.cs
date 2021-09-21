1ebf8816e0b4110205a3b72e70aa2282f69fa3c1
Shysh-Oleksandr <sasha.shysh23@gmail.com>
2021-09-21 14:42:28 +0300

    Optimized textures and made saving data into directory.

--- AstromaMazeMobile/Assets/Scripts/Controllers/SaveSystem.cs
+++ AstromaMazeMobile/Assets/Scripts/Controllers/SaveSystem.cs
@@ -6,19 +6,32 @@ public static class SaveSystem
 {
     public static void SaveGame(GameManager gameManager)
     {
-        BinaryFormatter formatter = new BinaryFormatter();
-        string path = GetPath("/gameMobile.dat");
-        using (FileStream stream = new FileStream(path, FileMode.Create))
-        {
-            GameData data = new GameData(gameManager);
+        string path = GetPath("gameMobile.sg");
 
-            formatter.Serialize(stream, data);
+        if (!Directory.Exists(Path.GetDirectoryName(path)))
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(path));
+        }
+
+        try
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = new FileStream(path, FileMode.Create);
+            GameData data = new GameData(gameManager);
+            bf.Serialize(file, data);
+            file.Close();
+            // Debug.Log("Saved Data to: " + path.Replace("/", "\\"));
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogWarning("Failed To Save Data to: " + path.Replace("/", "\\"));
+            Debug.LogWarning("Error: " + e.Message);
         }
     }
 
     public static GameData LoadGame()
     {
-        string path = GetPath("/gameMobile.dat");
+        string path = GetPath("gameMobile.sg");
 
         if (File.Exists(path))
         {
@@ -30,32 +43,46 @@ public static class SaveSystem
         }
         else
         {
-            // Debug.LogWarning("Save file not found in " + path);
+            Debug.LogWarning("Save file not found in " + path);
             return null;
         }
     }
-    
+
     public static void SaveItems(UpgradeManager upgradeManager)
     {
-        BinaryFormatter formatter = new BinaryFormatter();
-        string path = GetPath("/itemsMobile.dat");
-        using (FileStream stream = new FileStream(path, FileMode.Create))
-        {
-            ItemData data = new ItemData(upgradeManager);
+        string path = GetPath("itemsMobile.sg");
 
-            formatter.Serialize(stream, data);
+        if (!Directory.Exists(Path.GetDirectoryName(path)))
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(path));
+        }
+
+        try
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = new FileStream(path, FileMode.Create);
+            ItemData data = new ItemData(upgradeManager);
+            bf.Serialize(file, data);
+            file.Close();
+            // Debug.Log("Saved Data to: " + path.Replace("/", "\\"));
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogWarning("Failed To Save Data to: " + path.Replace("/", "\\"));
+            Debug.LogWarning("Error: " + e.Message);
         }
     }
 
     public static ItemData LoadItems()
     {
-        string path = GetPath("/itemsMobile.dat");
+        string path = GetPath("itemsMobile.sg");
 
         if (File.Exists(path))
         {
             BinaryFormatter formatter = new BinaryFormatter();
             using (FileStream stream = new FileStream(path, FileMode.Open))
             {
+
                 return formatter.Deserialize(stream) as ItemData;
             }
         }
@@ -68,50 +95,79 @@ public static class SaveSystem
 
     public static void SaveSettings(SettingsMenu settingsMenu)
     {
-        BinaryFormatter formatter = new BinaryFormatter();
-        string path = GetPath("/settingsMobile.dat");
-        using (FileStream stream = new FileStream(path, FileMode.Create))
-        {
-            SettingsData data = new SettingsData(settingsMenu);
+        string path = GetPath("settingsMobile.sg");
 
-            formatter.Serialize(stream, data);
+        if (!Directory.Exists(Path.GetDirectoryName(path)))
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(path));
         }
+
+        try
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = new FileStream(path, FileMode.Create);
+            SettingsData data = new SettingsData(settingsMenu);
+            bf.Serialize(file, data);
+            file.Close();
+            // Debug.Log("Saved Data to: " + path.Replace("/", "\\"));
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogWarning("Failed To Save Data to: " + path.Replace("/", "\\"));
+            Debug.LogWarning("Error: " + e.Message);
+        }
+
     }
 
     public static SettingsData LoadSettings()
     {
-        string path = GetPath("/settingsMobile.dat");
+        string path = GetPath("settingsMobile.sg");
 
         if (File.Exists(path))
         {
             BinaryFormatter formatter = new BinaryFormatter();
             using (FileStream stream = new FileStream(path, FileMode.Open))
             {
+
                 return formatter.Deserialize(stream) as SettingsData;
             }
         }
         else
         {
+
             // Debug.LogWarning("Save file not found in " + path);
             return null;
         }
     }
-    
+
     public static void SaveLanguage(LocalizationSetter localization)
     {
-        BinaryFormatter formatter = new BinaryFormatter();
-        string path = GetPath("/languageMobile.dat");
-        using (FileStream stream = new FileStream(path, FileMode.Create))
-        {
-            LanguageData data = new LanguageData(localization);
+        string path = GetPath("languageMobile.sg");
 
-            formatter.Serialize(stream, data);
+        if (!Directory.Exists(Path.GetDirectoryName(path)))
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(path));
+        }
+
+        try
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = new FileStream(path, FileMode.Create);
+            LanguageData data = new LanguageData(localization);
+            bf.Serialize(file, data);
+            file.Close();
+            // Debug.Log("Saved Data to: " + path.Replace("/", "\\"));
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogWarning("Failed To Save Data to: " + path.Replace("/", "\\"));
+            Debug.LogWarning("Error: " + e.Message);
         }
     }
 
     public static LanguageData LoadLanguage()
     {
-        string path = GetPath("/languageMobile.dat");
+        string path = GetPath("languageMobile.sg");
 
         if (File.Exists(path))
         {
@@ -122,14 +178,14 @@ public static class SaveSystem
             }
         }
         else
-        {
-            //Debug.LogWarning("Save file not found in " + path);
+        {   
             return null;
         }
     }
 
     private static string GetPath(string path)
     {
-        return Path.Combine(Application.persistentDataPath, path);
+        string filePath = Path.Combine(Application.persistentDataPath, "data");
+        return Path.Combine(filePath, path);
     }
 }
